4bb09529ce17eec9d1f1901c6d0be76d01410c4b
Hoàng Thanh Huy <21dh110638@st.huflit.edu.vn>
2023-10-03 00:31:34 +0700

    update

--- WebBanHangOnline/Areas/Admin/Controllers/ProductsController.cs
+++ WebBanHangOnline/Areas/Admin/Controllers/ProductsController.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
 
 namespace WebBanHangOnline.Areas.Admin.Controllers
 {
@@ -11,9 +13,26 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Admin/Products
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
+            IEnumerable<Product> items = db.Products.OrderByDescending(x => x.Id);
+            var pageSize = 10;
+            if (page == null)
+            {
+                page = 1;
+            }
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+            ViewBag.PageSize = pageSize;
+            ViewBag.Page = page;
+            return View(items);
+        }
+
+        public ActionResult Add()
+        {
+            ViewBag.ProductCategory = new SelectList(db.ProductCategories.ToList(), "Id", "Title");
             return View();
         }
+        
     }
 }
--- WebBanHangOnline/Models/EF/Product.cs
+++ WebBanHangOnline/Models/EF/Product.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +11,11 @@ namespace WebBanHangOnline.Models.EF
     [Table("tb_Product")]
     public class Product : CommonAbstract
     {
+        public Product()
+        {
+            this.ProductImage = new HashSet<ProductImage>();
+            this.OrderDetails = new HashSet<OrderDetail>();
+        }
         [Key]
         [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
@@ -23,7 +28,6 @@ namespace WebBanHangOnline.Models.EF
 
         [StringLength(50)]
         public string ProductCode { get; set; }
-        public int ProductCategoryID { get; set; }
         public string Description { get; set; }
 
         [AllowHtml]
@@ -31,21 +35,27 @@ namespace WebBanHangOnline.Models.EF
 
         [StringLength(250)]
         public string Image { get; set; }
+        public decimal OriginalPrice { get; set; }
         public decimal Price { get; set; }
-        public decimal PriceSale { get; set; }
+        public decimal? PriceSale { get; set; }
         public int Quantity { get; set; }
+        public int ViewCount { get; set; }
         public bool IsHome { get; set; }
         public bool IsSale { get; set; }
         public bool IsFeature { get; set; }
         public bool IsHot { get; set; }
         public bool IsActive { get; set; }
+        public int ProductCategoryID { get; set; }
+
         [StringLength(250)]
         public string SeoTitle { get; set; }
-        [StringLength(50)]
+        [StringLength(500)]
         public string SeoDiscription { get; set; }
         [StringLength(250)]
         public string SeoKeywords { get; set; }
 
         public virtual ProductCategory ProductCategory { get; set; }
+        public virtual ICollection<ProductImage> ProductImage { get; set; }
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
     }
 }
