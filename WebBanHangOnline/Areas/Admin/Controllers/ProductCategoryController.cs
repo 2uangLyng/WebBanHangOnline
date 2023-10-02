fc371a170be0226f53162b50da61cdbbb2c4fca7
Hoàng Thanh Huy <21dh110638@st.huflit.edu.vn>
2023-10-02 22:36:22 +0700

    update

---
+++ WebBanHangOnline/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.EF;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/ProductCategory
+        public ActionResult Index()
+        {
+            var items = db.ProductCategories;
+            return View(items);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(ProductCategory model)
+        {
+            if(ModelState.IsValid)
+            {
+                model.CreactedDate = DateTime.Now;                
+                model.ModifiedrDate = DateTime.Now;
+                model.Alias = WebBanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                db.ProductCategories.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
